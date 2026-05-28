70cb82e09ab91256d68424adbef696eff0244fb2
Arnaud TOUSSAINT <ArnaudToussaint@users.noreply.github.com>
2026-05-28 15:18:59 +0200

    Test

--- main.py
+++ main.py
@@ -1,74 +1,258 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
+from streamlit_calendar import calendar
 
-import requests
-import json
+st.set_page_config(page_title="Demo for streamlit-calendar", page_icon="📆")
 
+st.markdown(
+    "## Demo for [streamlit-calendar](https://github.com/im-perativa/streamlit-calendar) 📆"
+)
 
-class Reservation:
-    def __init__(self,name,date,time,num_people):
-        self.name  =name
-        self.date = date
-        self.time = time
-        self.num_people = num_people
+st.markdown(
+    "[![](https://img.shields.io/github/stars/im-perativa/streamlit-calendar?style=social)](https://github.com/im-perativa/streamlit-calendar)"
+)
 
-class ReservationApp:
-    def __init__(self):
-        self.reservations = []
+mode = st.selectbox(
+    "Calendar Mode:",
+    (
+        "daygrid",
+        "timegrid",
+        "timeline",
+        "resource-daygrid",
+        "resource-timegrid",
+        "resource-timeline",
+        "list",
+        "multimonth",
+    ),
+)
 
-    def add_reservation(self):
-        name = st.text_input("Name", value="Test anon")
-        date = st.date_input("Date")
-        time = st.time_input("Time")
-        num_people = int(st.number_input("Num people", min_value=int(1), value=int(1)))
+events = [
+    {
+        "title": "Event 1",
+        "color": "#FF6C6C",
+        "start": "2023-07-03",
+        "end": "2023-07-05",
+        "resourceId": "a",
+    },
+    {
+        "title": "Event 2",
+        "color": "#FFBD45",
+        "start": "2023-07-01",
+        "end": "2023-07-10",
+        "resourceId": "b",
+    },
+    {
+        "title": "Event 3",
+        "color": "#FF4B4B",
+        "start": "2023-07-20",
+        "end": "2023-07-20",
+        "resourceId": "c",
+    },
+    {
+        "title": "Event 4",
+        "color": "#FF6C6C",
+        "start": "2023-07-23",
+        "end": "2023-07-25",
+        "resourceId": "d",
+    },
+    {
+        "title": "Event 5",
+        "color": "#FFBD45",
+        "start": "2023-07-29",
+        "end": "2023-07-30",
+        "resourceId": "e",
+    },
+    {
+        "title": "Event 6",
+        "color": "#FF4B4B",
+        "start": "2023-07-28",
+        "end": "2023-07-20",
+        "resourceId": "f",
+    },
+    {
+        "title": "Event 7",
+        "color": "#FF4B4B",
+        "start": "2023-07-01T08:30:00",
+        "end": "2023-07-01T10:30:00",
+        "resourceId": "a",
+    },
+    {
+        "title": "Event 8",
+        "color": "#3D9DF3",
+        "start": "2023-07-01T07:30:00",
+        "end": "2023-07-01T10:30:00",
+        "resourceId": "b",
+    },
+    {
+        "title": "Event 9",
+        "color": "#3DD56D",
+        "start": "2023-07-02T10:40:00",
+        "end": "2023-07-02T12:30:00",
+        "resourceId": "c",
+    },
+    {
+        "title": "Event 10",
+        "color": "#FF4B4B",
+        "start": "2023-07-15T08:30:00",
+        "end": "2023-07-15T10:30:00",
+        "resourceId": "d",
+    },
+    {
+        "title": "Event 11",
+        "color": "#3DD56D",
+        "start": "2023-07-15T07:30:00",
+        "end": "2023-07-15T10:30:00",
+        "resourceId": "e",
+    },
+    {
+        "title": "Event 12",
+        "color": "#3D9DF3",
+        "start": "2023-07-21T10:40:00",
+        "end": "2023-07-21T12:30:00",
+        "resourceId": "f",
+    },
+    {
+        "title": "Event 13",
+        "color": "#FF4B4B",
+        "start": "2023-07-17T08:30:00",
+        "end": "2023-07-17T10:30:00",
+        "resourceId": "a",
+    },
+    {
+        "title": "Event 14",
+        "color": "#3D9DF3",
+        "start": "2023-07-17T09:30:00",
+        "end": "2023-07-17T11:30:00",
+        "resourceId": "b",
+    },
+    {
+        "title": "Event 15",
+        "color": "#3DD56D",
+        "start": "2023-07-17T10:30:00",
+        "end": "2023-07-17T12:30:00",
+        "resourceId": "c",
+    },
+    {
+        "title": "Event 16",
+        "color": "#FF6C6C",
+        "start": "2023-07-17T13:30:00",
+        "end": "2023-07-17T14:30:00",
+        "resourceId": "d",
+    },
+    {
+        "title": "Event 17",
+        "color": "#FFBD45",
+        "start": "2023-07-17T15:30:00",
+        "end": "2023-07-17T16:30:00",
+        "resourceId": "e",
+    },
+]
+calendar_resources = [
+    {"id": "a", "building": "Building A", "title": "Room A"},
+    {"id": "b", "building": "Building A", "title": "Room B"},
+    {"id": "c", "building": "Building B", "title": "Room C"},
+    {"id": "d", "building": "Building B", "title": "Room D"},
+    {"id": "e", "building": "Building C", "title": "Room E"},
+    {"id": "f", "building": "Building C", "title": "Room F"},
+]
 
-        if st.button("Make reservation"):
-            reservation = Reservation(name,date,time,num_people)
-            self.reservations.append(reservation)
+calendar_options = {
+    "editable": "true",
+    "navLinks": "true",
+    "resources": calendar_resources,
+    "selectable": "true",
+}
 
-            st.write(f"Reservation created for {name} on {date} at {time} for {num_people} people.")
-            st.balloons()
+if "resource" in mode:
+    if mode == "resource-daygrid":
+        calendar_options = {
+            **calendar_options,
+            "initialDate": "2023-07-01",
+            "initialView": "resourceDayGridDay",
+            "resourceGroupField": "building",
+        }
+    elif mode == "resource-timeline":
+        calendar_options = {
+            **calendar_options,
+            "headerToolbar": {
+                "left": "today prev,next",
+                "center": "title",
+                "right": "resourceTimelineDay,resourceTimelineWeek,resourceTimelineMonth",
+            },
+            "initialDate": "2023-07-01",
+            "initialView": "resourceTimelineDay",
+            "resourceGroupField": "building",
+        }
+    elif mode == "resource-timegrid":
+        calendar_options = {
+            **calendar_options,
+            "initialDate": "2023-07-01",
+            "initialView": "resourceTimeGridDay",
+            "resourceGroupField": "building",
+        }
+else:
+    if mode == "daygrid":
+        calendar_options = {
+            **calendar_options,
+            "headerToolbar": {
+                "left": "today prev,next",
+                "center": "title",
+                "right": "dayGridDay,dayGridWeek,dayGridMonth",
+            },
+            "initialDate": "2023-07-01",
+            "initialView": "dayGridMonth",
+        }
+    elif mode == "timegrid":
+        calendar_options = {
+            **calendar_options,
+            "initialView": "timeGridWeek",
+        }
+    elif mode == "timeline":
+        calendar_options = {
+            **calendar_options,
+            "headerToolbar": {
+                "left": "today prev,next",
+                "center": "title",
+                "right": "timelineDay,timelineWeek,timelineMonth",
+            },
+            "initialDate": "2023-07-01",
+            "initialView": "timelineMonth",
+        }
+    elif mode == "list":
+        calendar_options = {
+            **calendar_options,
+            "initialDate": "2023-07-01",
+            "initialView": "listMonth",
+        }
+    elif mode == "multimonth":
+        calendar_options = {
+            **calendar_options,
+            "initialView": "multiMonthYear",
+        }
 
-    def view_reservations(self):
-        if not self.reservations:
-            st.write(f"No reservation found")
-            return
-        for idx, res in enumerate(self.reservations, start=1):
-            st.write(f"Reservation ID={idx} : {res.name} on {res.date} at {res.time} for {res.num_people} people.")
+state = calendar(
+    events=st.session_state.get("events", events),
+    options=calendar_options,
+    custom_css="""
+    .fc-event-past {
+        opacity: 0.8;
+    }
+    .fc-event-time {
+        font-style: italic;
+    }
+    .fc-event-title {
+        font-weight: 700;
+    }
+    .fc-toolbar-title {
+        font-size: 2rem;
+    }
+    """,
+    key=mode,
+)
 
-    def cancel_reservation(self):
-        if not self.reservations:
-            st.write(f"No reservation to cancel")
-            return
-        self.view_reservations()
-        try:
-            choice = int(st.number_input("Enter the reservation ID to cancel"))
-            if 1 <= choice <= len(self.reservations):
-                canceled = self.reservations.pop(choice - 1)
-                st.write(f"Reservation canceled for {canceled.name} on {canceled.date} at {canceled.time} for {canceled.num_people} people.")
-            else:
-                st.write(f"Invalid choice, nothing to cancel")
-        except ValueError:
-            st.write(f"Invalid input, please enter a valid number")
+if state.get("eventsSet") is not None:
+    st.session_state["events"] = state["eventsSet"]
 
-    def run(self):
-        #while True:
-        st.write(f"Booking app")
-        if st.button("Add reservation"):
-            self.add_reservation()
-        if st.button("View reservations"):
-            self.view_reservations()
-        if st.button("Cancel reservation"):
-            self.cancel_reservation()
-        if st.button("Exit app"):
-            st.write(f"Exiting app, bye bye")
-            #break
+st.write(state)
 
-if st.button("Demo"):
-    st.switch_page("demo.py")
-
-if  __name__ == "__main__":
-    app = ReservationApp()
-    app.run()
-
+st.markdown("## API reference")
+st.help(calendar)
